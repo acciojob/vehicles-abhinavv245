de992d51bd37f4a6f3843499087cf0dac9e2b767
Abhinav <abhinavgoyal245@gmail.com>
2023-01-21 16:16:25 +0530

    added setters in boat class

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,16 +2,24 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
 
-    private String name="boat";
-    private int capacity=6;
+    private String name;
+    private int capacity;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
     @Override
     public String getVehicleName() {
-        return name;
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return capacity;
+        return this.capacity;
     }
 }
