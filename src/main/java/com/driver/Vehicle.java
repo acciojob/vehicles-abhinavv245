22bd35fdfa4bd2ca4d90560d3cfef9a3f582b7de
Abhinav <abhinavgoyal245@gmail.com>
2023-01-21 14:23:58 +0530

    driver code completed

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -11,6 +11,22 @@ public class Vehicle {
    this.currentDirection=0;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
     public int getCurrentSpeed() {
         return currentSpeed;
     }
