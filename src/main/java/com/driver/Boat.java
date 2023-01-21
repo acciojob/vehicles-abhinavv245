5fe5bcb637e35c1251e90315b923b5e46e6cf519
Abhinav <abhinavgoyal245@gmail.com>
2023-01-21 14:41:23 +0530

    boat class changed

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,33 +5,15 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
-    public Boat() {
 
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
 
     @Override
     public String getVehicleName() {
-        return this.getName();
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return this.getCapacity();
+        return this.capacity;
     }
 }
